7c2ba328ad2be07c319742121277eaa91766ee00
marlonlom <malm.gm@gmail.com>
2022-08-16 10:00:19 -0500

    added some typography

--- applications/mobile_app/src/main/kotlin/dev/marlonlom/apps/glucoreo/ui/theme/Type.kt
+++ applications/mobile_app/src/main/kotlin/dev/marlonlom/apps/glucoreo/ui/theme/Type.kt
@@ -31,29 +31,110 @@ val NunitoFont = FontFamily(
   Font(R.font.nunito_medium_italic, weight = FontWeight.Medium, style = FontStyle.Italic)
 )
 
-// Set of Material typography styles to start with
 val Typography = Typography(
+  displayLarge = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 57.sp,
+    lineHeight = 64.sp,
+    letterSpacing = 0.sp
+  ),
+  displayMedium = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 45.sp,
+    lineHeight = 52.sp,
+    letterSpacing = 0.sp
+  ),
+  displaySmall = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 36.sp,
+    lineHeight = 44.sp,
+    letterSpacing = 0.sp
+  ),
+  headlineLarge = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 32.sp,
+    lineHeight = 40.sp,
+    letterSpacing = 0.sp
+  ),
+  headlineMedium = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 28.sp,
+    lineHeight = 36.sp,
+    letterSpacing = 0.sp
+  ),
+  headlineSmall = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+    letterSpacing = 0.sp
+  ),
+  titleLarge = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 22.sp,
+    lineHeight = 28.sp,
+    letterSpacing = 0.sp
+  ),
+  titleMedium = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = (0.15).sp
+  ),
+  titleSmall = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (0.1).sp
+  ),
   bodyLarge = TextStyle(
     fontFamily = NunitoFont,
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp,
     lineHeight = 24.sp,
-    letterSpacing = 0.5.sp
-  )
-  /* Other default text styles to override
-  titleLarge = TextStyle(
-      fontFamily = FontFamily.Default,
-      fontWeight = FontWeight.Normal,
-      fontSize = 22.sp,
-      lineHeight = 28.sp,
-      letterSpacing = 0.sp
+    letterSpacing = (0.5).sp
+  ),
+  bodyMedium = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (0.25).sp
+  ),
+  bodySmall = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (0.4).sp
+  ),
+  labelLarge = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Medium,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = (0.1).sp
+  ),
+  labelMedium = TextStyle(
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (0.5).sp
   ),
   labelSmall = TextStyle(
-      fontFamily = FontFamily.Default,
-      fontWeight = FontWeight.Medium,
-      fontSize = 11.sp,
-      lineHeight = 16.sp,
-      letterSpacing = 0.5.sp
+    fontFamily = NunitoFont,
+    fontWeight = FontWeight.Medium,
+    fontSize = 11.sp,
+    lineHeight = 16.sp,
+    letterSpacing = (0.5).sp
   )
-  */
 )
