e7e634f70b3580fad4e35cfe812a42f8f6ac11ff
marlonlom <malm.gm@gmail.com>
2022-08-20 11:20:03 -0500

    updated buildSrc dependencies

--- buildSrc/src/main/kotlin/dev/marlonlom/apps/glucoreo/buildsrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/dev/marlonlom/apps/glucoreo/buildsrc/Dependencies.kt
@@ -1,4 +1,34 @@
-package dev.marlonlom.apps.glucoreo.buildSrc
+/*
+ * Copyright 2022 Marlonlom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.marlonlom.apps.glucoreo.buildsrc
+
+object Plugins {
+  const val androidGradle = "com.android.tools.build:gradle:7.2.2"
+  const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10"
+  const val detektGradle = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0"
+
+  const val androidApplicationPluginId = "com.android.application"
+  const val androidLibraryPluginId = "com.android.library"
+  const val kotlinAndroidPluginId = "org.jetbrains.kotlin.android"
+  const val spotlessPluginId = "com.diffplug.spotless"
+  const val ktlintPluginVersion = "0.45.2"
+  const val detektPluginId = "io.gitlab.arturbosch.detekt"
+  const val detektPluginVersion = "1.21.0"
+  const val kaptPluginId = "kotlin-kapt"
+}
 
 object Libs {
   object Implementation {
